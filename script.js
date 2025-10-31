2ea6489e8eee621a7fcf36fa4bb13dc572d6f10c
biscgames <fivesevenbengay@gmail.com>
2025-10-31 03:54:50 -0500

    got saucy with it

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-const minIconScale = 0.5;
-const maxIconScale = 0.6;
-
-let sectionContainer = document.getElementById("section-container");
-let buttonsContainer = document.getElementById("buttons-container");
-let section = "resume";
-
-function updateSectionContainer() {
-    for (let i in sectionContainer.children) {
-        const elem = sectionContainer.children[i];
-        const associatedImage = buttonsContainer.children[i];
-        if (associatedImage?.style) {
-            associatedImage.style.transform = "scale("+minIconScale+")";
-            associatedImage.addEventListener("click",()=>{
-                section = associatedImage.id;
-                updateSectionContainer();
-            });
-        } else continue;
-
-        elem.style.display = "none";
-        if (elem.id === section) {
-            elem.style.display = "flex";
-            associatedImage.style.transform = "scale("+maxIconScale+")";
-        }
+let stamps = [
+    {
+        innerText: "Hello! I am 16 years old and a programmer in Javascript, Lua!\nSo far i don't have much but one day i'll have an amazing resume!\n-"
+    },
+    {
+        innerText: "Stamps vv"
+    },
+    {
+        innerText: "I use arch btw.",
+        href: "https://archlinux.org/"
+    },
+    {
+        innerText: "Plasma: Simple by default, powerful when needed",
+        href: "https://kde.org/plasma-desktop/"
+    },
+    {
+        innerText: "JS is my favorite language and I'm not even lying. Array?.[1]??\"NO\" <3",
+        href: "https://ecma-international.org/publications-and-standards/standards/ecma-262/"
+    },
+    {
+        innerText: "LuaJIT is cool!",
+        href: "https://luajit.org/"
+    },
+    {
+        innerText: "Projects below vv"
+    },
+    {
+        innerText: "GoodForYou Animation",
+        href: "https://biscgames.github.io/goodforyou"
+    },
+    {
+        innerText: "WormySequences",
+        href: "https://biscgames.github.io/WormySequences"
+    },
+];
+for (let stamp of stamps) {
+    console.log(stamp)
+    const newStamp = document.createElement("a");
+    for (let attr in stamp) {
+        newStamp[attr]=stamp[attr];
     }
+    document.body.appendChild(newStamp);
+    newStamp.classList.add("normal");
+    if (!newStamp.href) continue;
+    newStamp.classList.remove("normal");
+    newStamp.classList.add("link");
+    newStamp.onmousedown = ()=>{
+        newStamp.classList.add("click");
+    };
+    let mouseupfunc = ()=>{
+        newStamp.classList.remove("click");
+    };
+    newStamp.onmouseup = mouseupfunc;
+    newStamp.onmouseleave = mouseupfunc;
 }
-
-updateSectionContainer();
